e2b59a7cbaaf2a388ce30b87a148865611030f24
Youljia <111038084+Youljia@users.noreply.github.com>
2022-09-24 20:31:44 +0800

    Add files via upload

---
+++ Priority_queue/Priority_queue/源.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include"Priority_queue.h"
+using namespace std;
+
+int main()
+{
+	vector<int> v1{ 1,2,3,4,5,6,7,10 };
+	Priority_queue queue(v1);
+	return 0;
+}
